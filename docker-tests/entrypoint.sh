74a77dc47b691eb5575d2b8a4ea7c80ce7252e10
Denys Davydov <dom8833@ya.ru>
2018-01-17 17:35:15 -0800

    NEW: TPT3-128. Add integration tests to test container. Commit 4. Added new required environment variables validation for integration tests

--- docker-tests/entrypoint.sh
+++ docker-tests/entrypoint.sh
@@ -16,15 +16,37 @@ if [[ "$TEST_TYPE" == "smoke" ]]; then
   echo "Executing the Smoke Test..."
   TEST_CLASS=gov.ca.cwds.cm.SmokeTestSuite
 elif [[ "$TEST_TYPE" == "integration" ]]; then
+  if ([ -z "$DB_CMS_JDBC_URL" ]); then
+    echo "DB_CMS_JDBC_URL variable is required"
+    exit 1
+  fi
+  if ([ -z "$DB_CMS_SCHEMA" ]); then
+    echo "DB_CMS_SCHEMA variable is required"
+    exit 1
+  fi
+  if ([ -z "$DB_CMS_USER" ]); then
+    echo "DB_CMS_USER variable is required"
+    exit 1
+  fi
+  if ([ -z "$DB_CMS_PASSWORD" ]); then
+    echo "DB_CMS_PASSWORD variable is required"
+    exit 1
+  fi
   echo "Executing the Integration Test..."
   TEST_CLASS=gov.ca.cwds.cm.IntegrationTestSuite
 else
   echo "Unknown TEST_TYPE: '$TEST_TYPE'"
+  echo "Known types: smoke, integration"
   exit 1
 fi
 
 echo "Starting tests: "
 echo "CASE_MANAGEMENT_API_URL = '$CASE_MANAGEMENT_API_URL'"
 echo "TEST_TYPE = '$TEST_TYPE'"
+echo "TEST_CLASS = '$TEST_CLASS'"
+echo "DB_CMS_JDBC_URL = '$DB_CMS_JDBC_URL'"
+echo "DB_CMS_SCHEMA = '$DB_CMS_SCHEMA'"
+echo "DB_CMS_USER = '$DB_CMS_USER'"
+echo "DB_CMS_PASSWORD = ********"
 
 java ${JAVA_OPT} -Dapi.url="${CASE_MANAGEMENT_API_URL}" -cp /opt/case-management-api-tests/resources:case-management-api-tests.jar org.junit.runner.JUnitCore ${TEST_CLASS}
