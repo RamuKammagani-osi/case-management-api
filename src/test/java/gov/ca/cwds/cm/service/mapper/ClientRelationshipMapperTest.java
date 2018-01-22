efd4edf0362cdd832b56b3b76493bbe5faa692f1
Dmytro Palczewski <dmytro.palczewski@HHSTechGroup.com>
2018-01-22 15:39:46 -0800

    TPT3-123: duplicated code is removed

--- src/test/java/gov/ca/cwds/cm/service/mapper/ClientRelationshipMapperTest.java
+++ src/test/java/gov/ca/cwds/cm/service/mapper/ClientRelationshipMapperTest.java
@@ -14,6 +14,23 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class ClientRelationshipMapperTest {
+
+  final private static String RELATIONSHIP_ID = "relationshipId";
+  final private static String LEFT_SIDE_CLIENT_ID = "leftSideClientId";
+  final private static String RIGHT_SIDE_CLIENT_ID = "rightSideClientId";
+  final private static String LEFT_SIDE_CLIENT_FIRST_NAME = "John";
+  final private static String LEFT_SIDE_CLIENT_LAST_NAME = "Left";
+  final private static String LEFT_SIDE_CLIENT_EMAIL = "left@left.com";
+  final private static String RIGHT_SIDE_CLIENT_FIRST_NAME = "Tomas";
+  final private static String RIGHT_SIDE_CLIENT_LAST_NAME = "Right";
+  final private static String RIGHT_SIDE_CLIENT_EMAIL = "right@right.com";
+  final private static LocalDate START_DATE = LocalDate.of(2012, 12, 22);
+  final private static LocalDate END_DATE = null;
+  final private static Short RELATIONSHIP_TYPE_SYSTEMID = (short)285;
+  final private static String RELATIONSHIP_TYPE_LONG_DESCRIPTION = "211";
+  final private static Boolean ABSENT_PARENT_INDICATOR = Boolean.FALSE;
+  final private static SameHomeStatus SAME_HOME_STATUS = SameHomeStatus.UNKNOWN;
+
   private ClientRelationshipMapperImpl mapper;
 
   @Before
@@ -23,43 +40,7 @@ public class ClientRelationshipMapperTest {
 
   @Test
   public void testFromRightRelationshipToDto() {
-
-    final String RELATIONSHIP_ID = "relationshipId";
-    final String LEFT_SIDE_CLIENT_ID = "leftSideClientId";
-    final String RIGHT_SIDE_CLIENT_ID = "rightSideClientId";
-    final String RIGHT_SIDE_CLIENT_FIRST_NAME = "Tomas";
-    final String RIGHT_SIDE_CLIENT_LAST_NAME = "Right";
-    final String RIGHT_SIDE_CLIENT_EMAIL = "right@right.com";
-    final LocalDate START_DATE = LocalDate.of(2012, 12, 22);
-    final LocalDate END_DATE = null;
-    final Short RELATIONSHIP_TYPE_SYSTEMID = (short)285;
-    final Boolean ABSENT_PARENT_INDICATOR = Boolean.FALSE;
-    final SameHomeStatus SAME_HOME_STATUS = SameHomeStatus.UNKNOWN;
-
-    ClientRelationship clientRelationship = new ClientRelationship();
-    clientRelationship.setIdentifier(RELATIONSHIP_ID);
-
-    Client leftSide = new Client();
-    leftSide.setIdentifier(LEFT_SIDE_CLIENT_ID);
-    clientRelationship.setLeftSide(leftSide);
-
-    Client rightSide = new Client();
-    rightSide.setIdentifier(RIGHT_SIDE_CLIENT_ID);
-    rightSide.setCommonFirstName(RIGHT_SIDE_CLIENT_FIRST_NAME);
-    rightSide.setCommonLastName(RIGHT_SIDE_CLIENT_LAST_NAME);
-    rightSide.setEmailAddress(RIGHT_SIDE_CLIENT_EMAIL);
-    clientRelationship.setRightSide(rightSide);
-
-    ClientRelationshipType clientRelationshipType = new ClientRelationshipType();
-    clientRelationshipType.setSystemId(RELATIONSHIP_TYPE_SYSTEMID);
-    clientRelationship.setType(clientRelationshipType);
-
-    clientRelationship.setAbsentParentIndicator(false);
-    clientRelationship.setStartDate(START_DATE);
-    clientRelationship.setEndDate(END_DATE);
-    clientRelationship.setSameHomeStatus(SameHomeStatus.UNKNOWN);
-
-    ClientRelationshipDTO dto = mapper.fromRightRelationshipToDto(clientRelationship);
+    ClientRelationshipDTO dto = mapper.fromRightRelationshipToDto(clientRelationship());
 
     assertNotNull(dto);
     assertEquals(RELATIONSHIP_ID, dto.getId());
@@ -81,23 +62,27 @@ public class ClientRelationshipMapperTest {
 
   @Test
   public void testFromLeftRelationshipToDto() {
+    ClientRelationshipDTO dto = mapper.fromLeftRelationshipToDto(clientRelationship());
 
-    final String RELATIONSHIP_ID = "relationshipId";
-    final String LEFT_SIDE_CLIENT_ID = "leftSideClientId";
-    final String RIGHT_SIDE_CLIENT_ID = "rightSideClientId";
-    final String LEFT_SIDE_CLIENT_FIRST_NAME = "John";
-    final String LEFT_SIDE_CLIENT_LAST_NAME = "Left";
-    final String LEFT_SIDE_CLIENT_EMAIL = "left@left.com";
-    final String RIGHT_SIDE_CLIENT_FIRST_NAME = "Tomas";
-    final String RIGHT_SIDE_CLIENT_LAST_NAME = "Right";
-    final String RIGHT_SIDE_CLIENT_EMAIL = "right@right.com";
-    final LocalDate START_DATE = LocalDate.of(2012, 12, 22);
-    final LocalDate END_DATE = null;
-    final Short RELATIONSHIP_TYPE_SYSTEMID = (short)285;
-    final String RELATIONSHIP_TYPE_LONG_DESCRIPTION = "211";
-    final Boolean ABSENT_PARENT_INDICATOR = Boolean.FALSE;
-    final SameHomeStatus SAME_HOME_STATUS = SameHomeStatus.UNKNOWN;
+    assertNotNull(dto);
+    assertEquals(RELATIONSHIP_ID, dto.getId());
+    assertEquals(RIGHT_SIDE_CLIENT_ID, dto.getClientId());
+    assertEquals(LEFT_SIDE_CLIENT_ID, dto.getRelatedClientId());
+    assertEquals(Short.valueOf((short)211), dto.getTypeCode());
+    assertEquals(ABSENT_PARENT_INDICATOR, dto.getAbsentParentIndicator());
+    assertEquals(START_DATE, dto.getStartDate());
+    assertEquals(END_DATE, dto.getEndDate());
+    assertEquals(SAME_HOME_STATUS.toString(), dto.getSameHomeStatus());
 
+    ClientDTO relatedClient = dto.getRelatedClient();
+    assertNotNull(relatedClient);
+    assertEquals(LEFT_SIDE_CLIENT_ID, relatedClient.getIdentifier());
+    assertEquals(LEFT_SIDE_CLIENT_FIRST_NAME, relatedClient.getCommonFirstName());
+    assertEquals(LEFT_SIDE_CLIENT_LAST_NAME, relatedClient.getCommonLastName());
+    assertEquals(LEFT_SIDE_CLIENT_EMAIL, relatedClient.getEmailAddr());
+  }
+
+  private static ClientRelationship clientRelationship() {
     ClientRelationship clientRelationship = new ClientRelationship();
     clientRelationship.setIdentifier(RELATIONSHIP_ID);
 
@@ -125,23 +110,6 @@ public class ClientRelationshipMapperTest {
     clientRelationship.setEndDate(END_DATE);
     clientRelationship.setSameHomeStatus(SameHomeStatus.UNKNOWN);
 
-    ClientRelationshipDTO dto = mapper.fromLeftRelationshipToDto(clientRelationship);
-
-    assertNotNull(dto);
-    assertEquals(RELATIONSHIP_ID, dto.getId());
-    assertEquals(RIGHT_SIDE_CLIENT_ID, dto.getClientId());
-    assertEquals(LEFT_SIDE_CLIENT_ID, dto.getRelatedClientId());
-    assertEquals(Short.valueOf((short)211), dto.getTypeCode());
-    assertEquals(ABSENT_PARENT_INDICATOR, dto.getAbsentParentIndicator());
-    assertEquals(START_DATE, dto.getStartDate());
-    assertEquals(END_DATE, dto.getEndDate());
-    assertEquals(SAME_HOME_STATUS.toString(), dto.getSameHomeStatus());
-
-    ClientDTO relatedClient = dto.getRelatedClient();
-    assertNotNull(relatedClient);
-    assertEquals(LEFT_SIDE_CLIENT_ID, relatedClient.getIdentifier());
-    assertEquals(LEFT_SIDE_CLIENT_FIRST_NAME, relatedClient.getCommonFirstName());
-    assertEquals(LEFT_SIDE_CLIENT_LAST_NAME, relatedClient.getCommonLastName());
-    assertEquals(LEFT_SIDE_CLIENT_EMAIL, relatedClient.getEmailAddr());
+    return clientRelationship;
   }
 }
