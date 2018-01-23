0561d88518e099ca2c3ad9e1717a488f6cd1a5fa
Dmytro Palczewski <dmytro.palczewski@HHSTechGroup.com>
2018-01-22 16:09:21 -0800

    TPT3-123: duplicated mapping is extracted

--- src/main/java/gov/ca/cwds/cm/service/mapper/ClientRelationshipMapper.java
+++ src/main/java/gov/ca/cwds/cm/service/mapper/ClientRelationshipMapper.java
@@ -2,6 +2,7 @@ package gov.ca.cwds.cm.service.mapper;
 
 import gov.ca.cwds.cm.service.dto.ClientRelationshipDTO;
 import gov.ca.cwds.data.legacy.cms.entity.ClientRelationship;
+import org.mapstruct.InheritConfiguration;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
@@ -9,27 +10,25 @@ import org.mapstruct.Mapping;
 public interface ClientRelationshipMapper {
 
   @Mapping(source = "identifier", target = "id")
-  @Mapping(source = "leftSide.identifier", target = "clientId")
-  @Mapping(source = "rightSide.identifier", target = "relatedClientId")
-  @Mapping(source = "type.systemId", target = "typeCode")
   @Mapping(source = "absentParentIndicator", target = "absentParentIndicator")
   @Mapping(source = "startDate", target = "startDate")
   @Mapping(source = "endDate", target = "endDate")
   @Mapping(source = "sameHomeStatus", target = "sameHomeStatus")
+  ClientRelationshipDTO fromRelationshipToDtoBase(ClientRelationship clientRelationship);
+
+  @InheritConfiguration(name = "fromRelationshipToDtoBase")
+  @Mapping(source = "leftSide.identifier", target = "clientId")
+  @Mapping(source = "rightSide.identifier", target = "relatedClientId")
+  @Mapping(source = "type.systemId", target = "typeCode")
   @Mapping(source = "rightSide", target = "relatedClient")
   ClientRelationshipDTO fromRightRelationshipToDto(ClientRelationship clientRelationship);
 
-  @Mapping(source = "identifier", target = "id")
+  @InheritConfiguration(name = "fromRelationshipToDtoBase")
   @Mapping(source = "rightSide.identifier", target = "clientId")
   @Mapping(source = "leftSide.identifier", target = "relatedClientId")
   @Mapping(
       expression = "java(Short.valueOf(clientRelationship.getType().getLongDescription()))",
       target = "typeCode")
-  @Mapping(source = "absentParentIndicator", target = "absentParentIndicator")
-  @Mapping(source = "startDate", target = "startDate")
-  @Mapping(source = "endDate", target = "endDate")
-  @Mapping(source = "sameHomeStatus", target = "sameHomeStatus")
   @Mapping(source = "leftSide", target = "relatedClient")
   ClientRelationshipDTO fromLeftRelationshipToDto(ClientRelationship clientRelationship);
-
 }
